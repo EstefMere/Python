3ffa0e01fee7c75f70e9bf7f4bf897e83f895a69
estefania.mejia <estefania.mejia@grupobrame.com>
2024-07-30 18:40:17 -0600

    subir cambios

---
+++ Debanando String.py
@@ -0,0 +1,10 @@
+cadena = "Este es un ejemplo de Substrin (Debanando Cadenas)"
+print(cadena[0:9])
+
+
+cadena = "Este es un ejemplo de Substrin (Debanando Cadenas)"
+print(cadena[20: ])
+
+
+cadena = "Este es un ejemplo de Substrin (Debanando Cadenas)"
+print(cadena[-1])
