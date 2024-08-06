b1e68b1b90ae6e0329f153dccf563f6e03c5a708
estefania.mejia <estefania.mejia@grupobrame.com>
2024-08-05 18:30:58 -0600

    Ejercicio cadenas

--- Debanando String.py
+++ Debanando String.py
@@ -1,10 +1,10 @@
-cadena = "Este es un ejemplo de Substrin (Debanando Cadenas)"
-print(cadena[0:9])
+cadena1 = "Este es un ejemplo de Substrin (Debanando Cadenas)"
+print(cadena1[0:9])
 
 
-cadena = "Este es un ejemplo de Substrin (Debanando Cadenas)"
-print(cadena[20: ])
+cadena2 = "Este es un ejemplo de Substrin (Debanando Cadenas)"
+print(cadena2[20: ])
 
-
-cadena = "Este es un ejemplo de Substrin (Debanando Cadenas)"
-print(cadena[-1])
+#Cuenta comienza desde atrás
+cadena3 = "Este es un ejemplo de Substrin (Debanando Cadenas)"
+print(cadena3[-1])
---
+++ Ejercicio1 Cadenas.py
@@ -0,0 +1,11 @@
+
+'''cadena = "Te quiero solo como amigo"
+print(cadena [0:2])'''
+
+
+cadena1 = "Estef"
+print(cadena1  [2: ])
+
+
+
+print(",".join("Hola mundo"))
